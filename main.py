ee824728e55c01a98b2f92444af6e10304a7d6e8
sebbbasdl <63890439+sebbbasdl@users.noreply.github.com>
2021-09-16 19:14:10 -0600

    commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from pagina import pagina
+from arbolb import Arbol_B
+
+if __name__ == '__main__':
+    arbolB = Arbol_B(5)
+    arbolB.insertar(1)
+    arbolB.insertar(2)
+    arbolB.insertar(3)
+    arbolB.insertar(4)
+    arbolB.insertar(5)
+
---
+++ pagina.py
@@ -0,0 +1,19 @@
+class pagina:
+    def __init__(self, orden):
+        self.cuenta = 0
+        self.m = orden
+        self.claves = [0 for x in range(orden)]
+        self.ramas = [pagina for x in range(orden)]
+
+        # inicializando las paginas
+
+        for i in range(orden):
+            self.ramas[i] = None
+
+    def pagina_llena(self):
+        return self.cuenta == self.m - 1
+
+    def pagina_semillena(self):
+        return self.cuenta < int(self.m / 2)
+
+
